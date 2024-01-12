bd061b86d802ab2e2024a218cbb77aabbbd870d3
errorbyme <amitk74019@gmail.com>
2024-01-12 16:54:46 +0530

    loading designe change

--- loader.php
+++ loader.php
@@ -1,10 +1,3 @@
 <div id="loader">
-<div class="spinner-grow text-primary" role="status"></div>
-<div class="spinner-grow text-secondary" role="status"></div>
-<div class="spinner-grow text-success" role="status"></div>
-<div class="spinner-grow text-danger" role="status"></div>
-<div class="spinner-grow text-warning" role="status"></div>
-<div class="spinner-grow text-info" role="status"></div>
-<div class="spinner-grow text-light" role="status"></div>
-<div class="spinner-grow text-dark" role="status"></div>
+    <img src="imgs/loading-giphy.gif" width="100px" alt="loading..">
 </div>
